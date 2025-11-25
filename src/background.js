7be60fb90e1e8230d5b4ef18b78317118b3d93c0
Jeff Hicken <jhicken@gmail.com>
2025-11-25 16:56:56 -0600

    add a keep alive

--- src/background.js
+++ src/background.js
@@ -4,10 +4,14 @@
 // Import modules
 import { setupCacheCleanup } from './modules/cacheManager.js';
 import { setupMessageHandlers } from './modules/messageHandler.js';
+import { initKeepAlive } from './modules/keepAlive.js';
 
 // Initialize the extension
-function init() {
+async function init() {
   console.log('Initializing Composer Quant Tools background script');
+  
+  // Setup keep-alive alarm
+  await initKeepAlive();
   
   // Setup cache cleanup
   setupCacheCleanup();
